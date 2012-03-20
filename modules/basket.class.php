cae250eb2f97cc9ac75702232854717078de2079
Ben Argo <ben@benargo.com>
2012-03-20 21:24:02 +0000

    Basket finished

--- modules/basket.class.php
+++ modules/basket.class.php
@@ -14,11 +14,32 @@ class basket {
 	// Construction function
 	public function __construct() {
 		
+		// Allow us to access sessions
+		session_start();
+		
 		// First include the database
 		global $db;
 		
 		// Check to see if we have a session
-		if($_SESSION['basket']) {
+		if(!isset($_SESSION['basket'])) {
+			
+			// Create a basket
+			$db->query("INSERT INTO `basket` (`paid`) VALUES (0)");
+
+			// Get the ID number of the basket
+			$basket_id = $db->insert_id;
+
+			// Set the ID number of the newly created basket to a session
+			$_SESSION['basket'] = $basket_id;
+
+			// Set the object variables
+			$this->id = $basket_id;
+			$this->paid = 0;
+
+			// Return true
+			return true;
+			
+		} else {
 			
 			// Run the database query
 			$sql = $db->query("SELECT * FROM `basket` WHERE `id` = ". $_SESSION['basket'] ." LIMIT 0, 1");
@@ -33,40 +54,37 @@ class basket {
 			// Return true
 			return true;
 			
-		} else {
-			
-			// Create a basket
-			$db->query("INSERT INTO `basket`");
-			
-			// Get the ID number of the basket
-			$basket_id = $db->insert_id;
-			
-			// Set the ID number of the newly created basket to a session
-			$_SESSION['basket'] = $basket_id;
-			
-			// Set the object variables
-			$this->id = $basket_id;
-			$this->paid = 0;
-			
-			// Return true
-			return true;
-			
 		}
 	}
 	
 	// Add an item to the basket
 	public function addItem($product, $quantity) {
 		
-		// Run the query
-		$db->query("INSERT INTO `basket_items` (`basket`, `product`, `quantity`) VALUES (". $this->id .", ". $product .", ". $quantity .")");
+		// Include the database details
+		global $db;
 		
-		// Return true
-		return true;
+		if($this->checkIfExists($product, $quantity)) {
+			
+			return true;
+			exit;
+			
+		} else {
+		
+			// Run the query
+			$db->query("INSERT INTO `basket_items` (`basket`, `product`, `quantity`) VALUES (". $this->id .", ". $product .", ". $quantity .")");
+		
+			// Return true
+			return true;
+		
+		}
 		
 	}
 	
 	// Remove an item from the basket
 	public function rmItem($id) {
+		
+		// Include the database details
+		global $db;
 	
 		// Run the query
 		$db->query("DELETE FROM `basket_items` WHERE `id` = $id");
@@ -78,6 +96,9 @@ class basket {
 	
 	// Change quantity of an item
 	public function changeQuantity($id, $quantity) {
+		
+		// Include the database details
+		global $db;
 	
 		// Run the query
 		$db->query("UPDATE `basket_items` SET `quantity` = $quantity WHERE `id` = $id");
@@ -90,22 +111,122 @@ class basket {
 	// Get the basket items
 	public function getItems() {
 		
+		// Include the database details
+		global $db;
+		
 		// Run the database query
 		$sql = $db->query("SELECT * FROM `basket_items` WHERE `basket` = ". $this->id);
 		
-		// Return the items
-		return $sql->fetch_object();
+		// Count the number of rows
+		if(count($sql->num_rows) <= 0) {
+			
+			// Redirect to the home page
+			header("location: ". BASE_URL);
+			
+			// Exit this script
+			exit;
+			
+		}
+		
+		// Declare a new array
+		$rows = Array();
+		
+		// Go through each of the rows
+		while($row = $sql->fetch_object()) {
+			
+			// Add each row to the end of the array
+			array_push($rows, $row);
+			
+		}
+		
+		// Finally return all the rows
+		return $rows;
 		
 	}
 	
 	// Set the basket as paid
 	public function paid() {
 		
+		// Include the database details
+		global $db; 
+		
 		// Set the basket as paid
 		$db->query("UPDATE `basket` SET `paid` = 1 WHERE `id` = ". $this->id);
 		
 		// Return true
 		return true;
+		
+	}
+	
+	// Check if the items are already in the basket
+	private function checkIfExists($product, $quantity) {
+		
+		// Include the database details
+		global $db;
+		
+		// Run the database query
+		$sql = $db->query("SELECT * FROM `basket_items` WHERE `basket` = ". $this->id ." AND `product` = $product LIMIT 0, 1");
+		
+		// If it already exists
+		if($sql->fetch_object()) {
+			
+			// Get the row id
+			$row_id = $sql->id;
+			
+			// Update the value
+			$db->query("UPDATE `basket_items` SET `quantity` = $quantity WHERE `id` = $row_id");
+			
+			// And return true
+			return true;
+			
+		} else {
+		
+			// Else return false
+			return false;
+			
+		}
+		
+	}
+	
+	// Calculate the final value of the basket
+	public function calcFinalValue() {
+		
+		// Include the product class (just in case)
+		require_once(BASE_URI.'modules/product.class.php');
+		
+		// Include the database details
+		global $db;
+		
+		// Create a new variable called price, and set it to 0.
+		$price = 0;
+		
+		// Get all of the items
+		$items = $this->getItems();
+		
+		// Run through all of the rows
+		foreach($items as $item) {
+			
+			// Create a new iteration of a product
+			$product = new product($item->product);
+			
+			// Find out the price 
+			$price = $price + ($product->price * $item->quantity);
+		}
+		
+		// Return the final price
+		return $price;
+		
+	}
+	
+	// Empty all the products in this basket
+	public function emptyBasket() {
+		
+		// Include the database details
+		global $db;
+		
+		// Run the first database query
+		// We want to remove all of the basket items
+		$db->query("DELETE FROM `basket_items` WHERE `basket` = ". $this->id);
 		
 	}
 }
--- pages/basket.page.php
+++ pages/basket.page.php
@@ -20,10 +20,113 @@ if (!defined('BASE_URL')) {
 
 // Include the product class
 require_once(BASE_URI.'modules/basket.class.php');
+require_once(BASE_URI.'modules/product.class.php');
 
 // Create a new product
 $basket = new basket();
-?>
-<h1>Basket</h1>
 
-<?php var_dump($_POST); ?>
+// Get the action
+$action = $_POST['a'];
+
+// Loop through the possible actions
+switch($action) {
+	
+	// Add products
+	case 'add':
+		
+		// Get some variables
+		$id = $_POST['id'];
+		$q = $_POST['q'];
+		
+		// Run the database query
+		$basket->addItem($id, $q) or die('Failed to add item to basket');
+		
+		// Finish this iteration
+		break;
+	
+	// Update the quantity
+	case 'edit':
+		
+		// Get some variables
+		$id = $_POST['id'];
+		$q = $_POST['q'];
+		
+		// Run the database query
+		$basket->changeQuantity($id, $q) or die('Failed to update basket');
+		
+		// Finish this iteration
+		break;
+		
+	// Remove the product
+	case 'rm':
+	
+		// Get some variables
+		$id = $_POST['id'];
+		
+		// Run the database query
+		$basket->rmItem($id);
+		
+		// Finish this iteration
+		break;
+		
+	// Empty the whole basket
+	case 'empty':
+	
+		// Run the database query
+		$basket->emptyBasket();
+		
+		// Finish this iteration
+		break;
+		
+	// If we didn't set a task (illegal call)
+	default:
+		
+		// Redirect to the home page
+		header("location: ". BASE_URL);
+		
+		exit;
+	
+		break;
+		// Finish this iteration
+}
+?><h1>Basket</h1><?php
+
+	// Loop through each of the basket items
+	foreach($basket->getItems() as $item) {
+		
+		// Get a product initialisation of each item
+		$product = new product($item->product);
+		
+		?><section class="product">
+			<h2><?php echo $product->title; ?></h2>
+			<img src="<?php echo BASE_URL; ?>images/<?php echo $product->image; ?>" alt="Photo of <?php echo $product->title; ?>" />
+			<div class="controls">
+				<form action="<?php echo BASE_URL; ?>basket" method="post">
+					<input type="hidden" name="a" value="edit" />
+					<input type="hidden" name="id" value="<?php echo $item->id; ?>" />
+					<p><select name="q"><?php
+					for($i = 1; $i <= 6; $i++) {
+						echo "<option";
+						if($i == $item->quantity) {
+							echo " selected";
+						}
+						echo ">$i</option>";
+					}
+					?></select> <input type="submit" value="Update" /></p>
+				</form>
+				<form action="<?php echo BASE_URL; ?>basket" method="post">
+					<input type="hidden" name="a" value="rm" />
+					<input type="hidden" name="id" value="<?php echo $item->id; ?>" />
+					<p><input type="submit" value="Remove" /></p>
+				</form>
+			</div>
+		</section><?php
+	}
+?><p>Total: <span class="bold">&pound;<?php echo $basket->calcFinalValue(); ?></span></p>
+<p class="center"><form action="<?php echo BASE_URL; ?>checkout" method="post">
+	<input type="hidden" name="stage" value="1" />
+	<input type="submit" value="Checkout" />
+</form><form action="<?php echo BASE_URL; ?>basket" method="post">
+	<input type="hidden" name="a" value="empty" />
+	<input type="submit" value="Empty Basket" />
+</form></p>
